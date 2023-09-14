34ceff93db305f6ce7abf312fdc55293795f4894
Kc-1212 <slidinganos@gmail.com>
2023-09-14 22:55:15 +0800

    speed formula

--- EffectHitRate.py
+++ EffectHitRate.py
@@ -1,19 +1,41 @@
+import math
 while True:
     try:
-        Real_Effect_Probability = 0
+        print("Menu:")
+        print("1. Calculate Real Effect Probability")
+        print("2. Speed vs AV per Round")
 
-        print("Real Effect Probability = Base Effect Probability x (1 + Attacker’s EHR) x (1 – Target’s Effect RES) x (1 – Target’s Debuff RES)\n\n\n\n",
-            "reference : https://honkai-star-rail.fandom.com/wiki/Effect_RES \n",
-            "Enemy Hit Res can be found in the website\n\n\n")
+        choice = input("Enter your choice (1 or 2): ")
 
-        Base_Effect_Probability = float(input("Enter the Base_Effect_Probability (Hit rate listed in skill description): "))
-        Attacker_EHR = float(input("Enter the Attacker_EHR (states the character): "))
-        Target_Effect_RES = float(input("Enter the Target_Effect_RES (Enemy's res state): "))
-        Target_Debuff_RES = float(input("Enter the Target_Debuff_RES: "))
+        if choice == '1':
+            Real_Effect_Probability = 0
 
-        Real_Effect_Probability = Base_Effect_Probability * (1 + Attacker_EHR / 100) * (1 - Target_Effect_RES / 100) * (1 - Target_Debuff_RES / 100)
+            print("Real Effect Probability = Base Effect Probability x (1 + Attacker’s EHR) x (1 – Target’s Effect RES) x (1 – Target’s Debuff RES)\n\n\n\n",
+                "reference : https://honkai-star-rail.fandom.com/wiki/Effect_RES \n",
+                "Enemy Hit Res can be found on the website\n\n\n")
 
-        print("Probability of taking effect:", Real_Effect_Probability)
+            Base_Effect_Probability = float(input("Enter the Base_Effect_Probability (Hit rate listed in skill description): "))
+            Attacker_EHR = float(input("Enter the Attacker_EHR (states the character): "))
+            Target_Effect_RES = float(input("Enter the Target_Effect_RES (Enemy's res state): "))
+            Target_Debuff_RES = float(input("Enter the Target_Debuff_RES: "))
+
+            Real_Effect_Probability = Base_Effect_Probability * (1 + Attacker_EHR / 100) * (1 - Target_Effect_RES / 100) * (1 - Target_Debuff_RES / 100)
+
+            print("Probability of taking effect:", Real_Effect_Probability)
+
+
+        elif choice == '2':
+
+            baseSPD=math.ceil(int(input("The speed of your character(without relics) :")))
+            SPDpercent=math.ceil(int(input("The speed percent from relics or buffs: ")))
+            FlatSPD=math.ceil(int(input("The speed from relics( flat speed without percentage) : ")))
+            Speed = math.ceil(baseSPD*((100+SPDpercent)/100)+FlatSPD)
+            print("speed : ", Speed)
+        
+        
+        
+        else:
+            print("Invalid choice. Please select a valid option (1 or 2).")
     except ZeroDivisionError:
         print("Error: Division by zero is not allowed.")
     except ValueError:
@@ -21,7 +43,7 @@ while True:
     except Exception as e:
         print(f"An error occurred: {e}")
 
-    user_input = input("Press 'q' to quit or any other key to continue: ")
+    user_input = input("Press 'q' to quit or empty to continue: ")
     if user_input.lower() == 'q':
         break
 
